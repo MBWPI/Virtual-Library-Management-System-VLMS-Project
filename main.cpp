7661303bd7a64619833826da7ec82d48b92cf1f6
MBWPI <MBWPI1405@gmail.com>
2024-05-26 01:46:55 -0400

    Added contributors

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+// Virtual Library Management System (VLMS) Project
+// Group 2 Group Project
+// Jackson Hopkins - Book Management II / Project Manager
+// Fadi Kanana - Data Persistence
+// Ian Laabs - Book Management I
+// Jonathan Jones - Interface and Navigation I
+// Michael Beland - Interface and Navigation II
+// Rachel Georges - User Account Management
+// Instructor: Nilan Karunaratne
+// Date: 05/23/2024
+
 #include <iostream>
 #include <string>
 #include <unordered_map>
